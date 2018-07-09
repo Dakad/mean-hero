0b914c941cd393fa57da584b77e08151d29a13d0
Dakad <bpottered+github@gmail.com>
2018-07-09 16:38:23 +0200

    FIx

--- src/server-start.js
+++ src/server-start.js
@@ -1,10 +1,3 @@
 require('babel-register');
 require('babel-polyfill');
-const WebApi = require('./web-api');
-
-WebApi.start()
-  .then(console.info)
-  .catch(e => {
-    console.error(e);
-    process.abort();
-  });
+require('./web-api');
--- src/web-api/index.js
+++ src/web-api/index.js
@@ -10,36 +10,32 @@ const Bluebird = require('bluebird'); // eslint-disable-line no-global-assign
 // plugin bluebird promise in mongoose
 mongoose.Promise = Bluebird;
 
-/**
- * Async function to init MongoDB and Express
- */
-export async function start() {
-  // connect to mongo db
-  const mongoUri = config.mongo.host;
-  let msg = '';
-  try {
-    await mongoose.connect(
-      mongoUri,
-      { keepAlive: 1 }
+// connect to mongo db
+const mongoUri = config.mongo.host;
+mongoose
+  .connect(
+    mongoUri,
+    { server: { socketOptions: { keepAlive: 1 } } }
+  )
+  .then(_ =>
+    console.info(`[MONGODB] Connected to ${mongoUri} (${config.env})`)
+  );
+
+mongoose.connection.on('error', () => {
+  throw new Error(`[MONGODB] Unable to connect to database : ${mongoUri}`);
+});
+
+// listen on port config.port
+app.listen(config.port, err => {
+  if (err && err.code === 'EADDRINUSE') {
+    throw new Error(
+      `[SERVER] Address ${config.port} already in use (${config.env})`
     );
-    msg = `[MONGODB] Connected to ${mongoUri}\n`;
-  } catch (error) {
-    throw new Error(`[MONGODB] Unable to connect to database : ${mongoUri}`);
   }
 
-  // listen on port config.port
-  try {
-    await Bluebird.promisify(app.listen);
-    return (
-      msg + `[SERVER] Server started on port ${config.port} (${config.env})`
-    );
-  } catch (err) {
-    if (err.code === 'EADDRINUSE') {
-      throw new Error(
-        `[SERVER] Address ${config.port} already in use (${config.env})`
-      );
-    } else {
-      throw err;
-    }
-  }
-}
+  console.info(
+    `[SERVER] Server started on port ${config.port} (${config.env})`
+  ); // eslint-disable-line no-console
+});
+
+export default app;
